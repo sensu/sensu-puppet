55dec6572ba07244f93c71fc357c76930040bafd
Tim Sharpe <tim@sharpe.id.au>
2012-04-29 11:41:52 +1000

    Type to manage rabbitmq config

---
+++ lib/puppet/provider/sensu_rabbitmq_config/json.rb
@@ -0,0 +1,116 @@
+require 'json'
+
+Puppet::Type.type(:sensu_rabbitmq_config).provide(:json) do
+  def initialize(*args)
+    super
+
+    @conf = JSON.parse(File.read('/etc/sensu/config.json'))
+  end
+
+  def flush
+    File.open('/etc/sensu/config.json', 'w') do |f|
+      f.puts JSON.pretty_generate(@conf)
+    end
+  end
+
+  def create
+    @conf['rabbitmq'] = {}
+    self.ssl_private_key = resource[:ssl_private_key] unless resource[:ssl_private_key].nil?
+    self.ssl_cert_chain = resource[:ssl_cert_chain] unless resource[:ssl_cert_chain].nil?
+    self.port = resource[:port]
+    self.host = resource[:host]
+    self.user = resource[:user]
+    self.password = resource[:password]
+    self.vhost = resource[:vhost]
+  end
+
+  def destroy
+    @conf.delete 'rabbitmq'
+  end
+
+  def exists?
+    @conf.has_key? 'rabbitmq'
+  end
+
+  def ssl_private_key
+    if @conf['rabbitmq'].has_key? 'ssl'
+      @conf['rabbitmq']['ssl']['private_key_file'] || ''
+    else
+      ''
+    end
+  end
+
+  def ssl_private_key=(value)
+    if value == ''
+      if @conf['rabbitmq'].has_key? 'ssl'
+        if @conf['rabbitmq']['ssl'].has_key? 'private_key_file'
+          @conf['rabbitmq']['ssl'].delete 'private_key_file'
+        end
+        @conf['rabbitmq'].delete 'ssl' if @conf['rabbitmq']['ssl'].empty?
+      end
+    else
+      (@conf['rabbitmq']['ssl'] ||= {})['private_key_file'] = value
+    end
+  end
+
+  def ssl_cert_chain
+    if @conf['rabbitmq'].has_key? 'ssl'
+      @conf['rabbitmq']['ssl']['cert_chain_file'] || ''
+    else
+      ''
+    end
+  end
+
+  def ssl_cert_chain=(value)
+    if value == ''
+      if @conf['rabbitmq'].has_key? 'ssl'
+        if @conf['rabbitmq']['ssl'].has_key? 'cert_chain_file'
+          @conf['rabbitmq']['ssl'].delete 'cert_chain_file'
+        end
+        @conf['rabbitmq'].delete 'ssl' if @conf['rabbitmq']['ssl'].empty?
+      end
+    else
+      (@conf['rabbitmq']['ssl'] ||= {})['cert_chain_file'] = value
+    end
+  end
+
+  def port
+    @conf['rabbitmq']['port'].to_s
+  end
+
+  def port=(value)
+    @conf['rabbitmq']['port'] = value.to_i
+  end
+
+  def host
+    @conf['rabbitmq']['host']
+  end
+
+  def host=(value)
+    @conf['rabbitmq']['host'] = value
+  end
+
+  def user
+    @conf['rabbitmq']['user']
+  end
+
+  def user=(value)
+    @conf['rabbitmq']['user'] = value
+  end
+
+  def password
+    @conf['rabbitmq']['password']
+  end
+
+  def password=(value)
+    @conf['rabbitmq']['password'] = value
+  end
+
+  def vhost
+    @conf['rabbitmq']['vhost']
+  end
+
+  def vhost=(value)
+    @conf['rabbitmq']['vhost'] = value
+  end
+end
---
+++ lib/puppet/type/sensu_rabbitmq_config.rb
@@ -0,0 +1,59 @@
+Puppet::Type.newtype(:sensu_rabbitmq_config) do
+  @doc = ""
+
+  ensurable do
+    newvalue(:present) do
+      provider.create
+    end
+
+    newvalue(:absent) do
+      provider.destroy
+    end
+
+    defaultto :present
+  end
+
+  newparam(:name) do
+    desc "This value has no effect, set it to what ever you want."
+  end
+
+  newproperty(:ssl_private_key) do
+    desc "The path on disk to the SSL private key needed to connect to RabbitMQ"
+
+    defaultto ''
+  end
+
+  newproperty(:ssl_cert_chain) do
+    desc "The path on disk to the SSL cert chain needed to connect to RabbitMQ"
+
+    defaultto ''
+  end
+
+  newproperty(:port) do
+    desc "The port that RabbitMQ is listening on"
+
+    defaultto '5671'
+  end
+
+  newproperty(:host) do
+    desc "The hostname that RabbitMQ is listening on"
+
+    defaultto 'localhost'
+  end
+
+  newproperty(:user) do
+    desc "The username to use when connecting to RabbitMQ"
+
+    defaultto 'sensu'
+  end
+
+  newproperty(:password) do
+    desc "The password to use when connecting to RabbitMQ"
+  end
+
+  newproperty(:vhost) do
+    desc "The vhost to use when connecting to RabbitMQ"
+
+    defaultto '/sensu'
+  end
+end
