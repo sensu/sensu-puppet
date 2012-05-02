0ff19d1886a173e1ca7cf8463a8fb2168f87fc0d
Tim Sharpe <tim@sharpe.id.au>
2012-05-02 18:14:08 +1000

    Lazy load check config file
    Only resource[:name] is set at initialize time, need to load the file after in
    order for exists? to work

--- lib/puppet/provider/sensu_check_config/json.rb
+++ lib/puppet/provider/sensu_check_config/json.rb
@@ -4,22 +4,26 @@ Puppet::Type.type(:sensu_check_config).provide(:json) do
   def initialize(*args)
     super
 
+    @conf = nil
+  end
+
+  def conf
     begin
-      @conf = JSON.parse(File.read("/etc/sensu/conf.d/checks_#{resource[:name]}.json"))
+      @conf ||= JSON.parse(File.read("/etc/sensu/conf.d/checks_#{resource[:realname]}.json"))
     rescue
-      @conf = {}
+      @conf ||= {}
     end
   end
 
   def flush
-    File.open("/etc/sensu/conf.d/checks_#{resource[:name]}.json", 'w') do |f|
-      f.puts JSON.pretty_generate(@conf)
+    File.open("/etc/sensu/conf.d/checks_#{resource[:realname]}.json", 'w') do |f|
+      f.puts JSON.pretty_generate(conf)
     end
   end
 
   def create
-    @conf['checks'] = {}
-    @conf['checks'][resource[:name]] = {}
+    conf['checks'] = {}
+    conf['checks'][resource[:realname]] = {}
     self.handlers = resource[:handlers]
     self.command = resource[:command]
     self.interval = resource[:interval]
@@ -27,42 +31,42 @@ Puppet::Type.type(:sensu_check_config).provide(:json) do
   end
 
   def destroy
-    @conf = nil
+    conf = nil
   end
 
   def exists?
-    @conf.has_key?('checks') and @conf['checks'].has_key?(resource[:name])
+    conf.has_key?('checks') and conf['checks'].has_key?(resource[:realname])
   end
 
   def interval
-    @conf['checks'][resource[:name]]['interval'].to_s
+    conf['checks'][resource[:realname]]['interval'].to_s
   end
 
   def interval=(value)
-    @conf['checks'][resource[:name]]['interval'] = value.to_i
+    conf['checks'][resource[:realname]]['interval'] = value.to_i
   end
 
   def handlers
-    @conf['checks'][resource[:name]]['handlers'] || []
+    conf['checks'][resource[:realname]]['handlers'] || []
   end
 
   def handlers=(value)
-    @conf['checks'][resource[:name]]['handlers'] = value
+    conf['checks'][resource[:realname]]['handlers'] = value
   end
 
   def command
-    @conf['checks'][resource[:name]]['command']
+    conf['checks'][resource[:realname]]['command']
   end
 
   def command=(value)
-    @conf['checks'][resource[:name]]['command'] = value
+    conf['checks'][resource[:realname]]['command'] = value
   end
 
   def subscribers
-    @conf['checks'][resource[:name]]['subscribers'] || []
+    conf['checks'][resource[:realname]]['subscribers'] || []
   end
 
   def subscribers=(value)
-    @conf['checks'][resource[:name]]['subscribers'] = value
+    conf['checks'][resource[:realname]]['subscribers'] = value
   end
 end
