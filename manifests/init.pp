a27c18a5087927ae00d9f168acf613f5304e08bc
Alexander Fortin <alexander.fortin@gmail.com>
2012-07-05 20:15:41 +0200

    Cleanup init manifest, debian APT repo not mandatory anymore

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,4 @@
 
 class sensu {
 
-  case $::operatingsystem {
-    # add apt sources
-    'Debian': { include 'sensu::debian' }
-    'Ubuntu': { include 'sensu::debian' }
-    default : {}
-  }
-
 }
