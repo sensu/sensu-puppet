2ad0e8fdb8d45302304c1a932bdc18208b4fafa1
Alexander Fortin <alexander.fortin@gmail.com>
2012-07-10 11:53:31 +0200

    Add yum.pp missing logic to handle yum repo

--- manifests/repo/yum.pp
+++ manifests/repo/yum.pp
@@ -3,4 +3,16 @@ class sensu::repo::yum (
     $repo
   ) {
 
+  $enabled = $ensure ? {
+    'present' => 1,
+    default   => 'absent'
+  }
+
+  yumrepo { 'sensu':
+    enabled  => $enabled,
+    baseurl  => 'http://repos.sensuapp.org/yum/el/$releasever/$basearch/',
+    gpgcheck => 0,
+    name     => "sensu-${repo}",
+  }
+
 }
