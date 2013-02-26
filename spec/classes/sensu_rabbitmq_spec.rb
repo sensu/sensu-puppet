c80029e740985d0e6ebc2f7fce22b545f833c543
Justin Lambert <jlambert@eml.cc>
2013-02-26 15:37:26 -0700

    cleaned up rabbitmq spec tests

--- spec/classes/sensu_rabbitmq_spec.rb
+++ spec/classes/sensu_rabbitmq_spec.rb
@@ -4,76 +4,67 @@ describe 'sensu::rabbitmq', :type => :class do
   let(:title) { 'myrabbit' }
   let(:facts) { { :fqdn => 'hostname.domain.com' } }
 
-  let(:params) { {
-    :ssl_cert_chain   => '/etc/sensu/ssl/chain.pem',
-    :ssl_private_key  => '/etc/sensu/ssl/key.pem',
-    :port             => '1234',
-    :host             => 'myhost',
-    :user             => 'sensuuser',
-    :password         => 'sensupass',
-    :vhost            => '/myvhost',
-    :notify_services  => []
-  } }
+  context 'when using local key' do
+    let(:params) { {
+      :ssl_cert_chain   => '/etc/private/ssl/cert.pem',
+      :ssl_private_key  => '/etc/private/ssl/key.pem',
+      :port             => '1234',
+      :host             => 'myhost',
+      :user             => 'sensuuser',
+      :password         => 'sensupass',
+      :vhost            => '/myvhost',
+      :notify_services  => []
+    } }
 
-  pending "it should test cert installs" do
-#    if $ssl_cert_chain != '' {
-#      file { '/etc/sensu/ssl':
-#        ensure => directory,
-#        owner  => 'sensu',
-#        group  => 'sensu',
-#        mode   => '0755',
-#        require => Package['sensu'],
-#      }
-#
-#      if $ssl_cert_chain =~ /^puppet:\/\// {
-#        file { '/etc/sensu/ssl/cert.pem':
-#          ensure  => present,
-#          source  => $ssl_cert_chain,
-#          owner   => 'sensu',
-#          group   => 'sensu',
-#          mode    => '0444',
-#          require => File['/etc/sensu/ssl'],
-#          before  => Sensu_rabbitmq_config[$::fqdn],
-#        }
-#
-#        Sensu_rabbitmq_config {
-#          ssl_cert_chain => '/etc/sensu/ssl/cert.pem',
-#        }
-#      } else {
-#        Sensu_rabbitmq_config {
-#          ssl_cert_chain => $ssl_cert_chain,
-#        }
-#      }
-#
-#      if $ssl_private_key =~ /^puppet:\/\// {
-#        file { '/etc/sensu/ssl/key.pem':
-#          ensure  => present,
-#          source  => $ssl_private_key,
-#          owner   => 'sensu',
-#          group   => 'sensu',
-#          mode    => '0440',
-#          require => File['/etc/sensu/ssl'],
-#          before  => Sensu_rabbitmq_config[$::fqdn],
-#        }
-#        Sensu_rabbitmq_config {
-#          ssl_private_key => '/etc/sensu/ssl/key.pem',
-#        }
-#      } else {
-#        Sensu_rabbitmq_config {
-#          ssl_private_key => $ssl_private_key,
-#        }
-#      }
+    it { should_not contain_file('/etc/sensu/ssl/cert.pem') }
+    it { should_not contain_file('/etc/sensu/ssl/key.pem') }
+
+    it { should contain_sensu_rabbitmq_config('hostname.domain.com').with(
+      'port'            => '1234',
+      'host'            => 'myhost',
+      'user'            => 'sensuuser',
+      'password'        => 'sensupass',
+      'vhost'           => '/myvhost',
+      'ssl_cert_chain'  => '/etc/private/ssl/cert.pem',
+      'ssl_private_key' => '/etc/private/ssl/key.pem'
+    ) }
   end
 
-  it { should contain_sensu_rabbitmq_config('hostname.domain.com').with(
-    'port'      => '1234',
-    'host'      => 'myhost',
-    'user'      => 'sensuuser',
-    'password'  => 'sensupass',
-    'vhost'     => '/myvhost'
-  ) }
+  context 'when using key in puppet' do
+    let(:params) { {
+      :ssl_cert_chain   => 'puppet:///modules/sensu/cert.pem',
+      :ssl_private_key  => 'puppet:///modules/sensu/key.pem',
+      :port             => '1234',
+      :host             => 'myhost',
+      :user             => 'sensuuser',
+      :password         => 'sensupass',
+      :vhost            => '/myvhost',
+      :notify_services  => ['Class[sensu::service::server]']
+    } }
+
+    it { should contain_file('/etc/sensu/ssl').with_ensure('directory') }
+    it { should contain_file('/etc/sensu/ssl/cert.pem').with_source('puppet:///modules/sensu/cert.pem') }
+    it { should contain_file('/etc/sensu/ssl/key.pem').with_source('puppet:///modules/sensu/key.pem') }
+
+    it { should contain_sensu_rabbitmq_config('hostname.domain.com').with(
+      'port'            => '1234',
+      'host'            => 'myhost',
+      'user'            => 'sensuuser',
+      'password'        => 'sensupass',
+      'vhost'           => '/myvhost',
+      'ssl_cert_chain'  => '/etc/sensu/ssl/cert.pem',
+      'ssl_private_key' => '/etc/sensu/ssl/key.pem',
+      'notify'          => 'Class[sensu::service::server]'
+    ) }
+  end
 
 end
+
+
+
+
+
+
 
 
 
