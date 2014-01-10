e5b7d6ee802472300c34579e3a8bd3c6939363fa
William Leese <william.leese@meltwater.com>
2014-01-10 15:54:15 +0900

    Allowing sensu user to be created by other means

--- manifests/package.pp
+++ manifests/package.pp
@@ -54,17 +54,19 @@ class sensu::package {
     require => Package['sensu'],
   }
 
-  user { 'sensu':
-    ensure  => 'present',
-    system  => true,
-    home    => '/opt/sensu',
-    shell   => '/bin/false',
-    comment => 'Sensu Monitoring Framework',
-  }
+  if $sensu::manage_user {
+    user { 'sensu':
+      ensure  => 'present',
+      system  => true,
+      home    => '/opt/sensu',
+      shell   => '/bin/false',
+      comment => 'Sensu Monitoring Framework',
+    }
 
-  group { 'sensu':
-    ensure  => 'present',
-    system  => true,
+    group { 'sensu':
+      ensure  => 'present',
+      system  => true,
+    }
   }
 
   file { '/etc/sensu/config.json': ensure => absent }
