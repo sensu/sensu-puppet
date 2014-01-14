6ebf86333de1289064f148ed633ee0a776b76a10
William Leese <william.leese@meltwater.com>
2014-01-14 11:47:00 +0900

    add spec tests for manage_user

--- spec/classes/sensu_init_spec.rb
+++ spec/classes/sensu_init_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe 'sensu', :type => :class do
 
   it 'should compile' do should create_class('sensu') end
+  it { should contain_user('sensu') }
+
+  context 'with manage_user => false' do
+    let(:params) { {:manage_user => false} }
+    it { should_not contain_user('sensu') }
+  end
 
 end
 
