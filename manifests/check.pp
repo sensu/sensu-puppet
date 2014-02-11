32f4d3ba14618c5c2443f92fda4950ebbd1fd11b
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-02-11 16:32:58 +0100

    Converted timeout to numeric

--- manifests/check.pp
+++ manifests/check.pp
@@ -41,7 +41,7 @@
 #   Default: undef
 #
 # [*timeout*]
-#   Integer.  Check timeout in seconds, after it fails
+#   Numeric.  Check timeout in seconds, after it fails
 #   Default: undef
 #
 # [*aggregate*]
@@ -86,8 +86,8 @@ define sensu::check(
   if $high_flap_threshold and !is_integer($high_flap_threshold) {
     fail("sensu::check{${name}}: high_flap_threshold must be an integer (got: ${high_flap_threshold})")
   }
-  if $timeout and !is_float($timeout) {
-    fail("sensu::check{${name}}: timeout must be an float (got: ${timeout})")
+  if $timeout and !is_numeric($timeout) {
+    fail("sensu::check{${name}}: timeout must be a numeric (got: ${timeout})")
   }
 
   $check_name = regsubst(regsubst($name, ' ', '_', 'G'), '[\(\)]', '', 'G')
