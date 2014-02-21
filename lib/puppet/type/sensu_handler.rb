161c8eb7671f0cf70c7c4a96fb1cde79458ba331
Dan Shultz <das0118@gmail.com>
2014-02-21 11:57:11 -0500

    fix updating handler socket => host value
    When updating a handler, the socket value only updated if the port had changed. It was also doing some verbose checking that wasn't future proof.
    
    This change includes a general spec along with using existing required method definitions to compare the `is` and `should` values

--- lib/puppet/type/sensu_handler.rb
+++ lib/puppet/type/sensu_handler.rb
@@ -44,20 +44,12 @@ Puppet::Type.newtype(:sensu_handler) do
       hash.keys.sort.map {|key| "#{key} => #{hash[key]}"}.join(", ")
     end
 
-    def should_to_s(hash = @should)
+    def should_to_s(hash = @should[0])
       hash.keys.sort.map {|key| "#{key} => #{hash[key]}"}.join(", ")
     end
 
     def insync?(is)
-      if defined? @should[0]
-        if is['port'] == (@should[0]['port'] = @should[0]['port'].to_i)
-          true
-        else
-          false
-        end
-      else
-        true
-      end
+      is_to_s(is) == should_to_s
     end
 
     defaultto {}
