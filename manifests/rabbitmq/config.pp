ef424d54aae8c5f042e3f66209625af3324dbfbf
dkiser <dkiser@godaddy.com>
2015-03-26 11:11:30 -0700

    adding ability to store rabbitmq cert/keys in hiera/vars instead of just file references.

--- manifests/rabbitmq/config.pp
+++ manifests/rabbitmq/config.pp
@@ -23,6 +23,8 @@ class sensu::rabbitmq::config {
       require => Package['sensu'],
     }
 
+    # if provided a cert chain, and its a puppet:// URI, source file form the
+    # the URI provided
     if $sensu::rabbitmq_ssl_cert_chain and $sensu::rabbitmq_ssl_cert_chain =~ /^puppet:\/\// {
       file { '/etc/sensu/ssl/cert.pem':
         ensure  => present,
@@ -35,10 +37,28 @@ class sensu::rabbitmq::config {
       }
 
       $ssl_cert_chain = '/etc/sensu/ssl/cert.pem'
+    # else provided a cert chain, and the variable actually contains the cert,
+    # create the file with conents of the variable
+    } elsif $sensu::rabbitmq_ssl_cert_chain and  $sensu::rabbitmq_ssl_cert_chain =~ /BEGIN CERTIFICATE/ {
+      file { '/etc/sensu/ssl/cert.pem':
+        ensure  => present,
+        content => $sensu::rabbitmq_ssl_cert_chain,
+        owner   => 'sensu',
+        group   => 'sensu',
+        mode    => '0444',
+        require => File['/etc/sensu/ssl'],
+        before  => Sensu_rabbitmq_config[$::fqdn],
+      }
+
+      $ssl_cert_chain = '/etc/sensu/ssl/cert.pem'
+    # else set the cert to value passed in wholesale, usually this is
+    # a raw file path
     } else {
       $ssl_cert_chain = $sensu::rabbitmq_ssl_cert_chain
     }
 
+    # if provided private key, and its a puppet:// URI, source file from the
+    # URI provided
     if $sensu::rabbitmq_ssl_private_key and $sensu::rabbitmq_ssl_private_key =~ /^puppet:\/\// {
       file { '/etc/sensu/ssl/key.pem':
         ensure  => present,
@@ -51,9 +71,26 @@ class sensu::rabbitmq::config {
       }
 
       $ssl_private_key = '/etc/sensu/ssl/key.pem'
+    # else provided private key, and the variable actually contains the key,
+    # create file with contents of the variable
+    } elsif $sensu::rabbitmq_ssl_private_key and $sensu::rabbitmq_ssl_private_key =~ /BEGIN RSA PRIVATE KEY/ {
+      file { '/etc/sensu/ssl/key.pem':
+        ensure  => present,
+        content => $sensu::rabbitmq_ssl_private_key,
+        owner   => 'sensu',
+        group   => 'sensu',
+        mode    => '0440',
+        require => File['/etc/sensu/ssl'],
+        before  => Sensu_rabbitmq_config[$::fqdn],
+      }
+
+      $ssl_private_key = '/etc/sensu/ssl/key.pem'
+    # else set the private key to value passed in wholesale, usually this is
+    # a raw file path
     } else {
       $ssl_private_key = $sensu::rabbitmq_ssl_private_key
     }
+
     $enable_ssl = true
   } else {
     $ssl_cert_chain = undef
