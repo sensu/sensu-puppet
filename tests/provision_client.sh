dd8796b8b987b3c8f1a59f17af7373499c1e5c52
Dominik Zyla <dominik.zyla@gmail.com>
2015-11-02 12:16:10 +0000

    Add ruby-dev to be installed whilst provisioning process.

--- tests/provision_client.sh
+++ tests/provision_client.sh
@@ -5,7 +5,7 @@
 # apt-get install -y python-software-properties
 wget --quiet http://apt.puppetlabs.com/puppetlabs-release-precise.deb -O /tmp/puppetlabs-release-precise.deb
 dpkg -i /tmp/puppetlabs-release-precise.deb
-apt-get update 
-apt-get install -y ruby-json puppet-common #masterless puppet
+apt-get update
+apt-get install -y ruby-json puppet-common ruby-dev #masterless puppet
 sed -i '/templatedir/d' /etc/puppet/puppet.conf
 puppet module install sensu/sensu
--- tests/provision_server.sh
+++ tests/provision_server.sh
@@ -5,8 +5,8 @@
 # apt-get install -y python-software-properties
 wget --quiet http://apt.puppetlabs.com/puppetlabs-release-precise.deb -O /tmp/puppetlabs-release-precise.deb
 dpkg -i /tmp/puppetlabs-release-precise.deb
-apt-get update 
-apt-get install -y ruby-json redis-server puppet-common #masterless puppet
+apt-get update
+apt-get install -y ruby-json redis-server puppet-common ruby-dev #masterless puppet
 sed -i '/templatedir/d' /etc/puppet/puppet.conf
 puppet module install sensu/sensu
 puppet module install puppetlabs/rabbitmq
