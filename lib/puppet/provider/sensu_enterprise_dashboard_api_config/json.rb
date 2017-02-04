9d9fb1898236ba790c9115a26208e60bef21110b
Garrett Honeycutt <code@garretthoneycutt.com>
2017-02-04 11:17:05 -0500

    Drop support for Ruby 1.9
    This patch removes support for Ruby 1.9.3. This module maintains
    compatibility with Puppet 3 which may run Ruby 2.0 and 2.1.
    
    Support for Ruby 1.8 was already removed as TravisCI was not testing for
    it. This patch also removes vestiges of that by no longer having the
    'require rubygems' lines.

--- lib/puppet/provider/sensu_enterprise_dashboard_api_config/json.rb
+++ lib/puppet/provider/sensu_enterprise_dashboard_api_config/json.rb
@@ -1,4 +1,3 @@
-require 'rubygems' if RUBY_VERSION < '1.9.0' && Puppet.version < '3'
 require 'json' if Puppet.features.json?
 require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..',
                                    'puppet_x', 'sensu', 'provider_create.rb'))
