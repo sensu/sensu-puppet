4f88ba73e66058a8ae0e51825a40b67212d6f66b
Andy Royle <ajroyle@gmail.com>
2017-02-27 17:53:25 +0000

    add x64 suffix to windows filename

--- manifests/package.pp
+++ manifests/package.pp
@@ -52,7 +52,7 @@ class sensu::package {
 
       remote_file { $pkg_source:
         ensure   => present,
-        source   => "http://repositories.sensuapp.org/msi/2012r2/sensu-${sensu::version}.msi",
+        source   => "http://repositories.sensuapp.org/msi/2012r2/sensu-${sensu::version}-x64.msi",
         checksum => $::sensu::package_checksum,
       }
     }
