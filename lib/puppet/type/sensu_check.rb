d4d7908f7376df7da8e1202b7355e1aa0aca7f8c
David Gwilliam <david.gwilliam@slalom.com>
2017-05-09 15:09:12 +0100

    check type should notify sensu-enterprise

--- lib/puppet/type/sensu_check.rb
+++ lib/puppet/type/sensu_check.rb
@@ -24,6 +24,7 @@ Puppet::Type.newtype(:sensu_check) do
     self[:notify] = [
       "Service[sensu-client]",
       "Service[sensu-server]",
+      "Service[sensu-enterprise]",
     ].select { |ref| catalog.resource(ref) }
   end
 
