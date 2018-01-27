74cef2da39635225ecfabfc6a3036ec15b3d441c
Joshua Knarr <Joshua.Knarr@LexisNexis.com>
2018-01-27 11:04:01 -0500

    I am misunderstanding something in this ruby

--- lib/puppet/provider/package/sensu_gem.rb
+++ lib/puppet/provider/package/sensu_gem.rb
@@ -16,7 +16,7 @@ Puppet::Type.type(:package).provide :sensu_gem, :parent => :gem do
       "#{ENV['SYSTEMDRIVE']}\\opt\\sensu\\embedded\\bin\\gem.cmd"
     else
       # grab the newest version available of the embedded ruby
-      embedded_rvm_version = Dir.glob(File.join(path, '/opt/sensu/embedded/lib/ruby/gems/*')).max { |a,b| File.ctime(a) <=> File.ctime(b) }
+      embedded_rvm_version = Dir.glob(File.join('/opt/sensu/embedded/lib/ruby/gems/', '*')).max { |a,b| File.ctime(a) <=> File.ctime(b) }
       ENV['GEM_PATH'] = "/opt/sensu/embedded/lib/ruby/gems/#{embedded_rvm_version}"
       "/opt/sensu/embedded/bin/gem"
     end
