69e6a13bc06c246d5b5938db19533904d3893c0a
Trey Dockendorf <treydock@gmail.com>
2018-04-18 17:46:37 -0400

    Force acceptance tests to use Puppet 5
    Version set by environment variable PUPPET_INSTALL_VERSION

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,8 +1,12 @@
 require 'beaker-rspec'
-require 'beaker/puppet_install_helper'
+# Helper does not yet support Puppet 5
+#require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
-run_puppet_install_helper
+# Helper does not yet support Puppet 5
+#install_puppetlabs_release_repo_on(hosts, 'puppet5')
+install_puppet_agent_on(hosts, :puppet_collection => 'puppet5', :puppet_agent_version => ENV['PUPPET_INSTALL_VERSION'])
+#run_puppet_install_helper
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
 
