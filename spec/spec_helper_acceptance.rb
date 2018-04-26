b3854a1d1971cb1df4646fa518e794a9c36d0384
Garrett Honeycutt <code@garretthoneycutt.com>
2018-04-26 19:37:56 -0400

    (GH-896) Support beaker testing on windows server 2012r2

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -24,6 +24,16 @@ RSpec.configure do |c|
         # CentOS has epel-release package in Extras, enabled by default
         shell('yum -y install epel-release')
       end
+      puts "platform is #{host['platform']}"
+      if host['platform'] =~ /windows/
+        require 'winrm'
+        include Serverspec::Helper::Windows
+        include Serverspec::Helper::WinRM
+
+        endpoint = "http://127.0.0.1:5985/wsman"
+        c.winrm = ::WinRM::WinRMWebService.new(endpoint, :ssl, :user => 'vagrant', :pass => 'vagrant', :basic_auth_only => true)
+        c.winrm.set_timeout 300
+      end
       on host, puppet('module', 'install', 'puppet-rabbitmq'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'fsalum-redis'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'puppetlabs-apt'), { :acceptable_exit_codes => [0,1] }
