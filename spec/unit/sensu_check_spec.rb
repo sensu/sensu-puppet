2cfd40d3664460e9db454854f4a815ccf5696038
Trey Dockendorf <treydock@gmail.com>
2018-05-06 18:13:11 -0400

    Make unit tests for new types a bit more generic so easier to add and modify properties all at once Used let based definition of types to make tests a bit more reliable

--- spec/unit/sensu_check_spec.rb
+++ spec/unit/sensu_check_spec.rb
@@ -2,14 +2,6 @@ require 'spec_helper'
 require 'puppet/type/sensu_check'
 
 describe Puppet::Type.type(:sensu_check) do
-  before(:each) do
-    @sensu_check = described_class.new(
-      name: 'test',
-      command: 'test',
-      subscriptions: ['test'],
-      handlers: ['test']
-    )
-  end
   let(:default_config) do
     {
       name: 'test',
@@ -28,7 +20,7 @@ describe Puppet::Type.type(:sensu_check) do
   it 'should add to catalog with raising an error' do
     catalog = Puppet::Resource::Catalog.new
     expect {
-      catalog.add_resource @sensu_check
+      catalog.add_resource check
     }.to_not raise_error
   end
 
@@ -38,276 +30,47 @@ describe Puppet::Type.type(:sensu_check) do
     }.to raise_error(Puppet::Error, 'Title or name must be provided')
   end
 
-  it 'should not accept invalid name' do
-    expect {
-      @sensu_check[:name] = 'foo bar'
-    }.to raise_error(Puppet::Error)
-  end
-
-  it 'should accept command' do
-    @sensu_check[:command] = '/foo/bar'
-    expect(@sensu_check[:command]).to eq('/foo/bar')
-  end
-
-  it 'should accept subscriptions' do
-    @sensu_check[:subscriptions] = ['foo', 'bar']
-    expect(@sensu_check[:subscriptions]).to eq(['foo', 'bar'])
-  end
-
-  it 'should accept handlers' do
-    @sensu_check[:handlers] = ['foo', 'bar']
-    expect(@sensu_check[:handlers]).to eq(['foo', 'bar'])
-  end
-
-  it 'should accept interval' do
-    @sensu_check[:interval] = 5
-    expect(@sensu_check[:interval]).to eq(5)
-  end
-
-  it 'should accept interval string' do
-    @sensu_check[:interval] = '5'
-    expect(@sensu_check[:interval]).to eq(5)
-  end
-
-  it 'should not accept invalid interval' do
-    expect {
-      @sensu_check[:interval] = 'foobar'
-    }.to raise_error(Puppet::ResourceError, /should be an Integer/)
-  end
-
-  it 'should accept cron' do
-    @sensu_check[:cron] = '0 0 * * *'
-    expect(@sensu_check[:cron]).to eq('0 0 * * *')
-  end
-
-  it 'should accept publish' do
-    @sensu_check[:publish] = true
-    expect(@sensu_check[:publish]).to eq(:true)
-    @sensu_check[:publish] = 'true'
-    expect(@sensu_check[:publish]).to eq(:true)
-  end
-
-  it 'should not accept invalid publish' do
-    expect {
-      @sensu_check[:publish] = 'foobar'
-    }.to raise_error(Puppet::ResourceError)
-  end
-
-  it 'should accept timeout' do
-    @sensu_check[:timeout] = 5
-    expect(@sensu_check[:timeout]).to eq(5)
-  end
-
-  it 'should accept timeout string' do
-    @sensu_check[:timeout] = '5'
-    expect(@sensu_check[:timeout]).to eq(5)
-  end
-
-  it 'should not accept invalid timeout' do
-    expect {
-      @sensu_check[:timeout] = 'foobar'
-    }.to raise_error(Puppet::ResourceError, /should be an Integer/)
-  end
-
-  it 'should accept ttl' do
-    @sensu_check[:ttl] = 5
-    expect(@sensu_check[:ttl]).to eq(5)
-  end
-
-  it 'should accept ttl string' do
-    @sensu_check[:ttl] = '5'
-    expect(@sensu_check[:ttl]).to eq(5)
-  end
-
-  it 'should not accept invalid ttl' do
-    expect {
-      @sensu_check[:ttl] = 'foobar'
-    }.to raise_error(Puppet::ResourceError, /should be an Integer/)
-  end
-
-  it 'should accept stdin' do
-    @sensu_check[:stdin] = true
-    expect(@sensu_check[:stdin]).to eq(:true)
-    @sensu_check[:stdin] = 'true'
-    expect(@sensu_check[:stdin]).to eq(:true)
-  end
-
-  it 'should not accept invalid stdin' do
-    expect {
-      @sensu_check[:stdin] = 'foobar'
-    }.to raise_error(Puppet::ResourceError)
-  end
-
-  it 'should accept low_flap_threshold' do
-    @sensu_check[:low_flap_threshold] = 5
-    expect(@sensu_check[:low_flap_threshold]).to eq(5)
-  end
-
-  it 'should accept low_flap_threshold string' do
-    @sensu_check[:low_flap_threshold] = '5'
-    expect(@sensu_check[:low_flap_threshold]).to eq(5)
-  end
-
-  it 'should not accept invalid low_flap_threshold' do
-    expect {
-      @sensu_check[:low_flap_threshold] = 'foobar'
-    }.to raise_error(Puppet::ResourceError, /should be an Integer/)
-  end
-
-  it 'should accept high_flap_threshold' do
-    @sensu_check[:high_flap_threshold] = 5
-    expect(@sensu_check[:high_flap_threshold]).to eq(5)
-  end
-
-  it 'should accept high_flap_threshold string' do
-    @sensu_check[:high_flap_threshold] = '5'
-    expect(@sensu_check[:high_flap_threshold]).to eq(5)
-  end
-
-  it 'should not accept invalid high_flap_threshold' do
-    expect {
-      @sensu_check[:high_flap_threshold] = 'foobar'
-    }.to raise_error(Puppet::ResourceError, /should be an Integer/)
-  end
-
-  it 'should accept runtime_assets' do
-    @sensu_check[:runtime_assets] = ['foo', 'bar']
-    expect(@sensu_check[:runtime_assets]).to eq(['foo', 'bar'])
-  end
-
-  it 'should accept check_hooks' do
-    @sensu_check[:check_hooks] = ['foo', 'bar']
-    expect(@sensu_check[:check_hooks]).to eq(['foo', 'bar'])
-  end
-
-  it 'should accept subdue' do
-    skip("Not sure what subdue looks like")
-  end
-
-  it 'should accept proxy_entity_id' do
-    @sensu_check[:proxy_entity_id] = 'foobar'
-    expect(@sensu_check[:proxy_entity_id]).to eq('foobar')
-  end
-
-  it 'should not accept invalid proxy_entity_id' do
-    expect {
-      @sensu_check[:proxy_entity_id] = 'foo bar'
-    }.to raise_error(Puppet::ResourceError)
-  end
-
-=begin
-  it 'should accept proxy_requests' do
-    @sensu_check[:proxy_requests] = {
-      "entity_attributes": ["entity.Class == 'proxy'"],
-      "splay": true,
-      "splay_coverage": 65
-    }
-    expect(@sensu_check[:proxy_requests]).to include("entity_attributes": ["entity.Class == 'proxy'"])
-    expect(@sensu_check[:proxy_requests]).to include("splay": true)
-    expect(@sensu_check[:proxy_requests]).to include("splay_coverage": 65)
-  end
-
-  it 'should not accept non-hash for proxy_requests' do
-    expect {
-      @sensu_check[:proxy_requests] = 'foobar'
-    }.to raise_error(Puppet::ResourceError)
-  end
-
-  it 'should not accept invalid keys for proxy_requests' do
-    expect {
-      @sensu_check[:proxy_requests] = {'foo': 'bar'}
-    }.to raise_error(Puppet::ResourceError)
-  end
-=end
-
-  it 'should accept round_robin' do
-    @sensu_check[:round_robin] = true
-    expect(@sensu_check[:round_robin]).to eq(:true)
-    @sensu_check[:round_robin] = 'true'
-    expect(@sensu_check[:round_robin]).to eq(:true)
-  end
-
-  it 'should not accept invalid round_robin' do
-    expect {
-      @sensu_check[:round_robin] = 'foobar'
-    }.to raise_error(Puppet::ResourceError)
-  end
-
-  it 'should have default organization' do
-    expect(@sensu_check[:organization]).to eq('default')
-  end
-
-  it 'should accept organization' do
-    @sensu_check[:organization] = 'foobar'
-    expect(@sensu_check[:organization]).to eq('foobar')
-  end
-
-  it 'should have default environment' do
-    expect(@sensu_check[:environment]).to eq('default')
-  end
-
-  it 'should accept environment' do
-    @sensu_check[:environment] = 'foobar'
-    expect(@sensu_check[:environment]).to eq('foobar')
-  end
-
-  it 'should accept proxy_requests_entity_attributes' do
-    @sensu_check[:proxy_requests_entity_attributes] = ['foo', 'bar']
-    expect(@sensu_check[:proxy_requests_entity_attributes]).to eq(['foo', 'bar'])
-  end
-
-  it 'should accept proxy_requests_splay' do
-    @sensu_check[:proxy_requests_splay] = true
-    expect(@sensu_check[:proxy_requests_splay]).to eq(:true)
-    @sensu_check[:proxy_requests_splay] = 'true'
-    expect(@sensu_check[:proxy_requests_splay]).to eq(:true)
-  end
-
-  it 'should not accept invalid proxy_requests_splay' do
-    expect {
-      @sensu_check[:proxy_requests_splay] = 'foobar'
-    }.to raise_error(Puppet::ResourceError)
-  end
-
-  it 'should accept proxy_requests_splay_coverage' do
-    @sensu_check[:proxy_requests_splay_coverage] = 5
-    expect(@sensu_check[:proxy_requests_splay_coverage]).to eq(5)
-  end
-
-  it 'should accept proxy_requests_splay_coverage string' do
-    @sensu_check[:proxy_requests_splay_coverage] = '5'
-    expect(@sensu_check[:proxy_requests_splay_coverage]).to eq(5)
-  end
-
-  it 'should not accept invalid proxy_requests_splay_coverage' do
-    expect {
-      @sensu_check[:proxy_requests_splay_coverage] = 'foobar'
-    }.to raise_error(Puppet::ResourceError, /should be an Integer/)
-  end
-
-  it 'should accept custom value' do
-    @sensu_check[:custom] = {'foo' => 'bar'}
-    expect(@sensu_check[:custom]).to eq({'foo' => 'bar'})
-  end
-
-  it 'should not accept invalid custom' do
-    expect {
-      @sensu_check[:custom] = 'foo'
-    }.to raise_error(Puppet::Error)
-  end
+  defaults = {
+    'organization': 'default',
+    'environment': 'default',
+  }
 
   # String properties
   [
+    :command,
+    :cron,
+    :proxy_entity_id,
     :metric_format
   ].each do |property|
     it "should accept valid #{property}" do
       config[property] = 'foo'
       expect(check[property]).to eq('foo')
     end
+    if default = defaults[property]
+      it "should have default for #{property}" do
+        expect(check[property]).to eq(default)
+      end
+    end
+  end
+
+  # String regex validated properties
+  [
+    :name,
+    :proxy_entity_id
+  ].each do |property|
+    it "should not accept invalid #{property}" do
+      config[property] = 'foo bar'
+      expect { check }.to raise_error(Puppet::Error)
+    end
   end
 
   # Array properties
   [
+    :subscriptions,
+    :handlers,
+    :runtime_assets,
+    :check_hooks,
+    :proxy_requests_entity_attributes,
     :metric_handlers
   ].each do |property|
     it "should accept valid #{property}" do
@@ -316,44 +79,110 @@ describe Puppet::Type.type(:sensu_check) do
     end
   end
 
+  # Integer properties
+  [
+    :interval,
+    :timeout,
+    :ttl,
+    :low_flap_threshold,
+    :high_flap_threshold,
+    :proxy_requests_splay_coverage,
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = 30
+      expect(check[property]).to eq(30)
+    end
+    it "should accept valid #{property} as string" do
+      config[property] = '30'
+      expect(check[property]).to eq(30)
+    end
+    it "should not accept invalid value for #{property}" do
+      config[property] = 'foo'
+      expect { check }.to raise_error(Puppet::Error, /should be an Integer/)
+    end
+  end
+
+  # Boolean properties
+  [
+    :publish,
+    :stdin,
+    :round_robin,
+    :proxy_requests_splay,
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = true
+      expect(check[property]).to eq(:true)
+    end
+    it "should accept valid #{property}" do
+      config[property] = false
+      expect(check[property]).to eq(:false)
+    end
+    it "should accept valid #{property}" do
+      config[property] = 'true'
+      expect(check[property]).to eq(:true)
+    end
+    it "should accept valid #{property}" do
+      config[property] = 'false'
+      expect(check[property]).to eq(:false)
+    end
+    it "should not accept invalid #{property}" do
+      config[property] = 'foo'
+      expect { check }.to raise_error(Puppet::Error)
+    end
+  end
+
+  # Hash properties
+  [
+    :custom
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = { 'foo': 'bar' }
+      expect(check[property]).to eq({'foo': 'bar'})
+    end
+    it "should not accept invalid #{property}" do
+      config[property] = 'foo'
+      expect { check }.to raise_error(Puppet::Error, /should be a Hash/)
+    end
+  end
+
   it 'should autorequire Package[sensu-cli]' do
     package = Puppet::Type.type(:package).new(:name => 'sensu-cli')
     catalog = Puppet::Resource::Catalog.new
-    catalog.add_resource @sensu_check
+    catalog.add_resource check
     catalog.add_resource package
-    rel = @sensu_check.autorequire[0]
+    rel = check.autorequire[0]
     expect(rel.source.ref).to eq(package.ref)
-    expect(rel.target.ref).to eq(@sensu_check.ref)
+    expect(rel.target.ref).to eq(check.ref)
   end
 
   it 'should autorequire Service[sensu-backend]' do
     service = Puppet::Type.type(:service).new(:name => 'sensu-backend')
     catalog = Puppet::Resource::Catalog.new
-    catalog.add_resource @sensu_check
+    catalog.add_resource check
     catalog.add_resource service
-    rel = @sensu_check.autorequire[0]
+    rel = check.autorequire[0]
     expect(rel.source.ref).to eq(service.ref)
-    expect(rel.target.ref).to eq(@sensu_check.ref)
+    expect(rel.target.ref).to eq(check.ref)
   end
 
   it 'should autorequire Exec[sensuctl_configure]' do
     exec = Puppet::Type.type(:exec).new(:name => 'sensuctl_configure', :command => '/usr/bin/sensuctl')
     catalog = Puppet::Resource::Catalog.new
-    catalog.add_resource @sensu_check
+    catalog.add_resource check
     catalog.add_resource exec
-    rel = @sensu_check.autorequire[0]
+    rel = check.autorequire[0]
     expect(rel.source.ref).to eq(exec.ref)
-    expect(rel.target.ref).to eq(@sensu_check.ref)
+    expect(rel.target.ref).to eq(check.ref)
   end
 
   it 'should autorequire sensu_api_validator' do
     validator = Puppet::Type.type(:sensu_api_validator).new(:name => 'sensu')
     catalog = Puppet::Resource::Catalog.new
-    catalog.add_resource @sensu_check
+    catalog.add_resource check
     catalog.add_resource validator
-    rel = @sensu_check.autorequire[0]
+    rel = check.autorequire[0]
     expect(rel.source.ref).to eq(validator.ref)
-    expect(rel.target.ref).to eq(@sensu_check.ref)
+    expect(rel.target.ref).to eq(check.ref)
   end
 
   [
--- spec/unit/sensu_handler_spec.rb
+++ spec/unit/sensu_handler_spec.rb
@@ -2,32 +2,26 @@ require 'spec_helper'
 require 'puppet/type/sensu_handler'
 
 describe Puppet::Type.type(:sensu_handler) do
-  before(:each) do
-    @sensu_handler = described_class.new(
-      name: 'test',
-      type: 'pipe',
-      command: 'test'
-    )
-  end
-
   let(:default_config) do
     {
       name: 'test',
       type: 'pipe',
       command: 'test',
+      socket_host: '127.0.0.1',
+      socket_port: 9000,
     }
   end
   let(:config) do
     default_config
   end
-  let(:check) do
+  let(:handler) do
     described_class.new(config)
   end
 
   it 'should add to catalog without raising an error' do
     catalog = Puppet::Resource::Catalog.new
     expect {
-      catalog.add_resource @sensu_handler
+      catalog.add_resource handler
     }.to_not raise_error
   end
 
@@ -37,148 +31,159 @@ describe Puppet::Type.type(:sensu_handler) do
     }.to raise_error(Puppet::Error, 'Title or name must be provided')
   end
 
-  it 'should not accept invalid name' do
-    expect {
-      @sensu_handler[:name] = 'foo bar'
-    }.to raise_error(Puppet::Error)
-  end
-
   it 'should accept type' do
-    @sensu_handler[:type] = 'tcp'
-    expect(@sensu_handler[:type]).to eq(:tcp)
+    handler[:type] = 'tcp'
+    expect(handler[:type]).to eq(:tcp)
   end
 
   it 'should not accept invalid type' do
     expect {
-      @sensu_handler[:type] = 'foo'
+      handler[:type] = 'foo'
     }.to raise_error(Puppet::Error)
   end
 
-  it 'should accept filters' do
-    @sensu_handler[:filters] = ['foo', 'bar']
-    expect(@sensu_handler[:filters]).to eq(['foo', 'bar'])
+  defaults = {
+    'organization': 'default',
+    'environment': 'default',
+  }
+
+  # String properties
+  [
+    :mutator,
+    :command,
+    :organization,
+    :environment,
+    :socket_host,
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = 'foo'
+      expect(handler[property]).to eq('foo')
+    end
+    if default = defaults[property]
+      it "should have default for #{property}" do
+        expect(handler[property]).to eq(default)
+      end
+    end
   end
 
-  it 'should accept mutator' do
-    @sensu_handler[:mutator] = 'foo'
-    expect(@sensu_handler[:mutator]).to eq('foo')
+  # String regex validated properties
+  [
+    :name,
+  ].each do |property|
+    it "should not accept invalid #{property}" do
+      config[property] = 'foo bar'
+      expect { handler }.to raise_error(Puppet::Error)
+    end
   end
 
-  it 'should accept timeout' do
-    @sensu_handler[:timeout] = 5
-    expect(@sensu_handler[:timeout]).to eq(5)
+  # Array properties
+  [
+    :filters,
+    :env_vars,
+    :handlers,
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = ['foo', 'bar']
+      expect(handler[property]).to eq(['foo', 'bar'])
+    end
   end
 
-  it 'should accept timeout string' do
-    @sensu_handler[:timeout] = '5'
-    expect(@sensu_handler[:timeout]).to eq(5)
+  # Integer properties
+  [
+    :timeout,
+    :socket_port,
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = 30
+      expect(handler[property]).to eq(30)
+    end
+    it "should accept valid #{property} as string" do
+      config[property] = '30'
+      expect(handler[property]).to eq(30)
+    end
+    it "should not accept invalid value for #{property}" do
+      config[property] = 'foo'
+      expect { handler }.to raise_error(Puppet::Error, /should be an Integer/)
+    end
   end
 
-  it 'should not accept invalid timeout' do
-    expect {
-      @sensu_handler[:timeout] = 'foobar'
-    }.to raise_error(Puppet::ResourceError, /should be an Integer/)
+  # Boolean properties
+  [
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = true
+      expect(handler[property]).to eq(:true)
+    end
+    it "should accept valid #{property}" do
+      config[property] = false
+      expect(handler[property]).to eq(:false)
+    end
+    it "should accept valid #{property}" do
+      config[property] = 'true'
+      expect(handler[property]).to eq(:true)
+    end
+    it "should accept valid #{property}" do
+      config[property] = 'false'
+      expect(handler[property]).to eq(:false)
+    end
+    it "should not accept invalid #{property}" do
+      config[property] = 'foo'
+      expect { handler }.to raise_error(Puppet::Error)
+    end
   end
 
-  it 'should accept command' do
-    @sensu_handler[:command] = '/foo/bar'
-    expect(@sensu_handler[:command]).to eq('/foo/bar')
-  end
-
-  it 'should accept env_vars' do
-    @sensu_handler[:env_vars] = ['foo', 'bar']
-    expect(@sensu_handler[:env_vars]).to eq(['foo', 'bar'])
-  end
-
-  it 'should accept handlers' do
-    @sensu_handler[:handlers] = ['foo', 'bar']
-    expect(@sensu_handler[:handlers]).to eq(['foo', 'bar'])
-  end
-
-  it 'should accept organization' do
-    @sensu_handler[:organization] = 'foobar'
-    expect(@sensu_handler[:organization]).to eq('foobar')
-  end
-
-  it 'should have default environment' do
-    expect(@sensu_handler[:environment]).to eq('default')
-  end
-
-  it 'should accept environment' do
-    @sensu_handler[:environment] = 'foobar'
-    expect(@sensu_handler[:environment]).to eq('foobar')
-  end
-
-  it 'should accept socket_host' do
-    @sensu_handler[:socket_host] = 'foo'
-    expect(@sensu_handler[:socket_host]).to eq('foo')
-  end
-
-  it 'should accept socket_port' do
-    @sensu_handler[:socket_port] = 5
-    expect(@sensu_handler[:socket_port]).to eq(5)
-  end
-
-  it 'should accept socket_port string' do
-    @sensu_handler[:socket_port] = '5'
-    expect(@sensu_handler[:socket_port]).to eq(5)
-  end
-
-  it 'should not accept invalid socket_port' do
-    expect {
-      @sensu_handler[:socket_port] = 'foobar'
-    }.to raise_error(Puppet::ResourceError, /should be an Integer/)
-  end
-
-  it 'should accept custom value' do
-    @sensu_handler[:custom] = {'foo' => 'bar'}
-    expect(@sensu_handler[:custom]).to eq({'foo' => 'bar'})
-  end
-
-  it 'should not accept invalid custom' do
-    expect {
-      @sensu_handler[:custom] = 'foo'
-    }.to raise_error(Puppet::Error)
+  # Hash properties
+  [
+    :custom
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = { 'foo': 'bar' }
+      expect(handler[property]).to eq({'foo': 'bar'})
+    end
+    it "should not accept invalid #{property}" do
+      config[property] = 'foo'
+      expect { handler }.to raise_error(Puppet::Error, /should be a Hash/)
+    end
   end
 
   it 'should autorequire Package[sensu-cli]' do
     package = Puppet::Type.type(:package).new(:name => 'sensu-cli')
     catalog = Puppet::Resource::Catalog.new
-    catalog.add_resource @sensu_handler
+    catalog.add_resource handler
     catalog.add_resource package
-    rel = @sensu_handler.autorequire[0]
+    rel = handler.autorequire[0]
     expect(rel.source.ref).to eq(package.ref)
-    expect(rel.target.ref).to eq(@sensu_handler.ref)
+    expect(rel.target.ref).to eq(handler.ref)
   end
 
   it 'should autorequire Service[sensu-backend]' do
     service = Puppet::Type.type(:service).new(:name => 'sensu-backend')
     catalog = Puppet::Resource::Catalog.new
-    catalog.add_resource @sensu_handler
+    catalog.add_resource handler
     catalog.add_resource service
-    rel = @sensu_handler.autorequire[0]
+    rel = handler.autorequire[0]
     expect(rel.source.ref).to eq(service.ref)
-    expect(rel.target.ref).to eq(@sensu_handler.ref)
+    expect(rel.target.ref).to eq(handler.ref)
   end
 
   it 'should autorequire Exec[sensuctl_configure]' do
     exec = Puppet::Type.type(:exec).new(:name => 'sensuctl_configure', :command => '/usr/bin/sensuctl')
     catalog = Puppet::Resource::Catalog.new
-    catalog.add_resource @sensu_handler
+    catalog.add_resource handler
     catalog.add_resource exec
-    rel = @sensu_handler.autorequire[0]
+    rel = handler.autorequire[0]
     expect(rel.source.ref).to eq(exec.ref)
-    expect(rel.target.ref).to eq(@sensu_handler.ref)
+    expect(rel.target.ref).to eq(handler.ref)
   end
 
   it 'should autorequire sensu_api_validator' do
     validator = Puppet::Type.type(:sensu_api_validator).new(:name => 'sensu')
     catalog = Puppet::Resource::Catalog.new
-    catalog.add_resource @sensu_handler
+    catalog.add_resource handler
     catalog.add_resource validator
-    rel = @sensu_handler.autorequire[0]
+    rel = handler.autorequire[0]
     expect(rel.source.ref).to eq(validator.ref)
-    expect(rel.target.ref).to eq(@sensu_handler.ref)
+    expect(rel.target.ref).to eq(handler.ref)
   end
 
   [
