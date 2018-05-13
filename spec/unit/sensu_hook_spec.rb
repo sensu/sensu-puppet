ea1b43a6f86ff02d0b9c4c1485bce4f891b776b1
Trey Dockendorf <treydock@gmail.com>
2018-05-13 13:58:37 -0400

    Add sensu_hook type/provider

---
+++ spec/unit/sensu_hook_spec.rb
@@ -0,0 +1,184 @@
+require 'spec_helper'
+require 'puppet/type/sensu_hook'
+
+describe Puppet::Type.type(:sensu_hook) do
+  let(:default_config) do
+    {
+      name: 'test',
+      command: 'test',
+    }
+  end
+  let(:config) do
+    default_config
+  end
+  let(:hook) do
+    described_class.new(config)
+  end
+
+  it 'should add to catalog without raising an error' do
+    catalog = Puppet::Resource::Catalog.new
+    expect {
+      catalog.add_resource hook
+    }.to_not raise_error
+  end
+
+  it 'should require a name' do
+    expect {
+      described_class.new({})
+    }.to raise_error(Puppet::Error, 'Title or name must be provided')
+  end
+
+  defaults = {
+    'organization': 'default',
+    'environment': 'default',
+    'timeout': 60,
+  }
+
+  # String properties
+  [
+    :command,
+    :organization,
+    :environment,
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = 'foo'
+      expect(hook[property]).to eq('foo')
+    end
+    if default = defaults[property]
+      it "should have default for #{property}" do
+        expect(hook[property]).to eq(default)
+      end
+    end
+  end
+
+  # String regex validated properties
+  [
+    :name,
+  ].each do |property|
+    it "should not accept invalid #{property}" do
+      config[property] = 'foo bar'
+      expect { hook }.to raise_error(Puppet::Error)
+    end
+  end
+
+  # Array properties
+  [
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = ['foo', 'bar']
+      expect(hook[property]).to eq(['foo', 'bar'])
+    end
+  end
+
+  # Integer properties
+  [
+    :timeout,
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = 30
+      expect(hook[property]).to eq(30)
+    end
+    it "should accept valid #{property} as string" do
+      config[property] = '30'
+      expect(hook[property]).to eq(30)
+    end
+    it "should not accept invalid value for #{property}" do
+      config[property] = 'foo'
+      expect { hook }.to raise_error(Puppet::Error, /should be an Integer/)
+    end
+    if default = defaults[property]
+      it "should have default for #{property}" do
+        expect(hook[property]).to eq(default)
+      end
+    end
+  end
+
+  # Boolean properties
+  [
+    :stdin
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = true
+      expect(hook[property]).to eq(:true)
+    end
+    it "should accept valid #{property}" do
+      config[property] = false
+      expect(hook[property]).to eq(:false)
+    end
+    it "should accept valid #{property}" do
+      config[property] = 'true'
+      expect(hook[property]).to eq(:true)
+    end
+    it "should accept valid #{property}" do
+      config[property] = 'false'
+      expect(hook[property]).to eq(:false)
+    end
+    it "should not accept invalid #{property}" do
+      config[property] = 'foo'
+      expect { hook }.to raise_error(Puppet::Error)
+    end
+  end
+
+  # Hash properties
+  [
+  ].each do |property|
+    it "should accept valid #{property}" do
+      config[property] = { 'foo': 'bar' }
+      expect(hook[property]).to eq({'foo': 'bar'})
+    end
+    it "should not accept invalid #{property}" do
+      config[property] = 'foo'
+      expect { hook }.to raise_error(Puppet::Error, /should be a Hash/)
+    end
+  end
+
+  it 'should autorequire Package[sensu-cli]' do
+    package = Puppet::Type.type(:package).new(:name => 'sensu-cli')
+    catalog = Puppet::Resource::Catalog.new
+    catalog.add_resource hook
+    catalog.add_resource package
+    rel = hook.autorequire[0]
+    expect(rel.source.ref).to eq(package.ref)
+    expect(rel.target.ref).to eq(hook.ref)
+  end
+
+  it 'should autorequire Service[sensu-backend]' do
+    service = Puppet::Type.type(:service).new(:name => 'sensu-backend')
+    catalog = Puppet::Resource::Catalog.new
+    catalog.add_resource hook
+    catalog.add_resource service
+    rel = hook.autorequire[0]
+    expect(rel.source.ref).to eq(service.ref)
+    expect(rel.target.ref).to eq(hook.ref)
+  end
+
+  it 'should autorequire Exec[sensuctl_configure]' do
+    exec = Puppet::Type.type(:exec).new(:name => 'sensuctl_configure', :command => '/usr/bin/sensuctl')
+    catalog = Puppet::Resource::Catalog.new
+    catalog.add_resource hook
+    catalog.add_resource exec
+    rel = hook.autorequire[0]
+    expect(rel.source.ref).to eq(exec.ref)
+    expect(rel.target.ref).to eq(hook.ref)
+  end
+
+  it 'should autorequire sensu_api_validator' do
+    validator = Puppet::Type.type(:sensu_api_validator).new(:name => 'sensu')
+    catalog = Puppet::Resource::Catalog.new
+    catalog.add_resource hook
+    catalog.add_resource validator
+    rel = hook.autorequire[0]
+    expect(rel.source.ref).to eq(validator.ref)
+    expect(rel.target.ref).to eq(hook.ref)
+  end
+
+  [
+    :command,
+  ].each do |property|
+    it "should require property when ensure => present" do
+      config.delete(property)
+      config[:ensure] = :present
+      expect { hook }.to raise_error(Puppet::Error, /You must provide a #{property}/)
+    end
+  end
+end
