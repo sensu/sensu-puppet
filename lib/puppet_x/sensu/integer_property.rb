9651b895bb063f86e1f3b3136a526a06c98f3857
Trey Dockendorf <treydock@gmail.com>
2018-05-23 20:30:20 -0400

    Integer type supports negative integers

--- lib/puppet_x/sensu/integer_property.rb
+++ lib/puppet_x/sensu/integer_property.rb
@@ -2,7 +2,7 @@ module PuppetX
   module Sensu
     class IntegerProperty < Puppet::Property
       validate do |value|
-        unless value.to_s =~ /^\d+$/ or value.to_s == 'absent'
+        unless value.to_s =~ /^[-]?\d+$/ or value.to_s == 'absent'
           raise ArgumentError, "#{self.name.to_s} should be an Integer"
         end
       end
