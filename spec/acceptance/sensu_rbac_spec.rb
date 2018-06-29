19d134bc883dbd649693378732d8ad6237a1ea15
Trey Dockendorf <treydock@gmail.com>
2018-06-29 19:33:54 -0400

    Bundle RBAC related acceptance tests to improve runtime

--- spec/acceptance/sensu_rbac_spec.rb
+++ spec/acceptance/sensu_rbac_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper_acceptance'
 
-describe 'sensu_role' do
+describe 'sensu RBAC resources' do
   node = only_host_with_role(hosts, 'sensu_backend')
   context 'default' do
     it 'should work without errors' do
       pp = <<-EOS
       include ::sensu::backend
+      sensu_environment { 'test':
+        description => 'test',
+      }
+      sensu_organization { 'test':
+        description => 'test',
+      }
       sensu_role { 'test':
         rules => [{'type' => '*', 'environment' => '*', 'organization' => '*', 'permissions' => ['read']}]
       }
@@ -16,6 +22,20 @@ describe 'sensu_role' do
       apply_manifest_on(node, pp, :catch_changes  => true)
     end
 
+    it 'should have a valid environment' do
+      on node, 'sensuctl environment list --format json' do
+        data = JSON.parse(stdout)
+        d = data.select { |o| o['name'] == 'test' }
+        expect(d[0]['name']).to eq('test')
+      end
+    end
+    it 'should have a valid organization' do
+      on node, 'sensuctl organization list --format json' do
+        data = JSON.parse(stdout)
+        d = data.select { |o| o['name'] == 'test' }
+        expect(d[0]['name']).to eq('test')
+      end
+    end
     it 'should have a valid role' do
       on node, 'sensuctl role list --format json' do
         data = JSON.parse(stdout)
@@ -25,10 +45,16 @@ describe 'sensu_role' do
     end
   end
 
-  context 'update role' do
+  context 'update environment' do
     it 'should work without errors' do
       pp = <<-EOS
       include ::sensu::backend
+      sensu_environment { 'test':
+        description => 'foo',
+      }
+      sensu_organization { 'test':
+        description => 'foo',
+      }
       sensu_role { 'test':
         rules => [{'type' => '*', 'environment' => '*', 'organization' => '*', 'permissions' => ['read', 'create']}],
       }
@@ -39,6 +65,20 @@ describe 'sensu_role' do
       apply_manifest_on(node, pp, :catch_changes  => true)
     end
 
+    it 'should have a valid environment with updated propery' do
+      on node, 'sensuctl environment list --format json' do
+        data = JSON.parse(stdout)
+        d = data.select { |o| o['name'] == 'test' }
+        expect(d[0]['description']).to eq('foo')
+      end
+    end
+    it 'should have a valid organization with updated propery' do
+      on node, 'sensuctl organization list --format json' do
+        data = JSON.parse(stdout)
+        d = data.select { |o| o['name'] == 'test' }
+        expect(d[0]['description']).to eq('foo')
+      end
+    end
     it 'should have a valid role with updated propery' do
       on node, 'sensuctl role list --format json' do
         data = JSON.parse(stdout)
@@ -53,6 +93,8 @@ describe 'sensu_role' do
     it 'should remove without errors' do
       pp = <<-EOS
       include ::sensu::backend
+      sensu_environment { 'test': ensure => 'absent' }
+      sensu_organization { 'test': ensure => 'absent' }
       sensu_role { 'test': ensure => 'absent' }
       EOS
 
@@ -61,6 +103,20 @@ describe 'sensu_role' do
       apply_manifest_on(node, pp, :catch_changes  => true)
     end
 
+    it 'should not have test environment' do
+      on node, 'sensuctl environment list --format json' do
+        data = JSON.parse(stdout)
+        d = data.select { |o| o['name'] == 'test' }
+        expect(d.size).to eq(0)
+      end
+    end
+    it 'should not have test organization' do
+      on node, 'sensuctl organization list --format json' do
+        data = JSON.parse(stdout)
+        d = data.select { |o| o['name'] == 'test' }
+        expect(d.size).to eq(0)
+      end
+    end
     it 'should not have test role' do
       on node, 'sensuctl role list --format json' do
         data = JSON.parse(stdout)
