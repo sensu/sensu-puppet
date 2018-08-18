5772a3f5378945c45f57f679eb0e75b7765ff04a
Trey Dockendorf <treydock@gmail.com>
2018-08-18 17:29:23 -0400

    Update acceptance tests to test check_hooks

--- spec/acceptance/sensu_check_spec.rb
+++ spec/acceptance/sensu_check_spec.rb
@@ -11,6 +11,9 @@ describe 'sensu_check', if: RSpec.configuration.sensu_full do
         subscriptions => ['demo'],
         handlers      => ['email'],
         interval      => 60,
+        check_hooks   => [
+          { 'critical' => ['httpd-restart'] },
+        ],
       }
       EOS
 
@@ -23,6 +26,7 @@ describe 'sensu_check', if: RSpec.configuration.sensu_full do
       on node, 'sensuctl check info test --format json' do
         data = JSON.parse(stdout)
         expect(data['command']).to eq('check-http.rb')
+        expect(data['check_hooks']).to eq([{'critical' => ['httpd-restart']}])
       end
     end
   end
