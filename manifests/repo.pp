b0aa87c09488b0a41a71278cb6ec2e31c6c7ac1c
Massimiliano Adamo <massimiliano.adamo@geant.org>
2019-02-11 12:53:54 +0100

    skip repo if manage_repo set to false
    remove the inherits and use include

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,48 +1,52 @@
-# private class
+# @summary Private class to manage sensu repository resources
+# @api private
+#
 class sensu::repo {
 
-  case $facts['os']['family'] {
-    'RedHat': {
-      if $facts['os']['name'] == 'Amazon' {
-        if $facts['os']['release']['major'] =~ /^201\d$/ {
-          $repo_release = '6'
+  include ::sensu
+
+  if $sensu::manage_repo {
+    case $facts['os']['family'] {
+      'RedHat': {
+        if $facts['os']['name'] == 'Amazon' {
+          if $facts['os']['release']['major'] =~ /^201\d$/ {
+            $repo_release = '6'
+          } else {
+            $repo_release = '7'
+          }
         } else {
-          $repo_release = '7'
+          $repo_release = $facts['os']['release']['major']
         }
-      } else {
-        $repo_release = $facts['os']['release']['major']
+        yumrepo { 'sensu':
+          descr           => 'sensu',
+          baseurl         => "https://packagecloud.io/sensu/stable/el/${repo_release}/\$basearch",
+          repo_gpgcheck   => 1,
+          gpgcheck        => 0,
+          enabled         => 1,
+          gpgkey          => 'https://packagecloud.io/sensu/stable/gpgkey',
+          sslverify       => 1,
+          sslcacert       => '/etc/pki/tls/certs/ca-bundle.crt',
+          metadata_expire => 300,
+        }
       }
-      # TODO: change from beta to stable once there are stable releases
-      yumrepo { 'sensu':
-        descr           => 'sensu',
-        baseurl         => "https://packagecloud.io/sensu/beta/el/${repo_release}/\$basearch",
-        repo_gpgcheck   => 1,
-        gpgcheck        => 0,
-        enabled         => 1,
-        gpgkey          => 'https://packagecloud.io/sensu/beta/gpgkey',
-        sslverify       => 1,
-        sslcacert       => '/etc/pki/tls/certs/ca-bundle.crt',
-        metadata_expire => 300,
+      'Debian': {
+        apt::source { 'sensu':
+          ensure   => 'present',
+          location => "https://packagecloud.io/sensu/stable/${downcase($facts['os']['name'])}/",
+          repos    => 'main',
+          release  => $facts['os']['distro']['codename'],
+          include  => {
+            'src' => true,
+          },
+          key      => {
+            'id'     => 'CB1605C4E988C91F438249E3A5BC3FB70A3F7426',
+            'source' => 'https://packagecloud.io/sensu/stable/gpgkey',
+          },
+        }
       }
-    }
-    'Debian': {
-      #TODO: change from beta to stable once there are stable releases
-      apt::source { 'sensu':
-        ensure   => 'present',
-        location => "https://packagecloud.io/sensu/beta/${downcase($facts['os']['name'])}/",
-        repos    => 'main',
-        release  => $facts['os']['distro']['codename'],
-        include  => {
-          'src' => true,
-        },
-        key      => {
-          'id'     => '0B3B86AFEF2D99B085BEDC6A4263180AAE8AAE03',
-          'source' => 'https://packagecloud.io/sensu/beta/gpgkey',
-        },
+      default: {
+        # do nothing
       }
-    }
-    default: {
-      # do nothing
     }
   }
 }
