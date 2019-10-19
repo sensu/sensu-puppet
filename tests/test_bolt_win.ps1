bf5623473be3044ddc5671af88c0496da6bbdbe6
Trey Dockendorf <treydock@gmail.com>
2019-10-19 10:35:10 -0400

    Code cleanup

--- tests/test_bolt_win.ps1
+++ tests/test_bolt_win.ps1
@@ -1,7 +1,4 @@
-#$env:PATH += ";C:\Program Files\sensu\sensu-agent\bin"
 $env:PATH += ";C:\Program Files\Puppet Labs\Puppet\bin"
-#iex 'sensu-agent.exe service uninstall'
-#iex 'puppet resource package "Sensu Agent" ensure=absent'
 iex "puppet module install puppetlabs-chocolatey"
 iex "puppet apply -e 'include ::chocolatey'"
 $env:PATH += ";C:\ProgramData\chocolatey\bin"
