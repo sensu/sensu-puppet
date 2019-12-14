16c7b186711080722a72f94175c17ac42835bbe8
Trey Dockendorf <treydock@gmail.com>
2019-12-14 09:40:42 -0500

    Add sensu_api provider for sensu_oidc_auth

--- lib/puppet/provider/sensu_oidc_auth/sensu_api.rb
+++ lib/puppet/provider/sensu_oidc_auth/sensu_api.rb
@@ -1,26 +1,20 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'sensuctl'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'sensu_api'))
 
-Puppet::Type.type(:sensu_oidc_auth).provide(:sensuctl, :parent => Puppet::Provider::Sensuctl) do
-  desc "Provider sensu_oidc_auth using sensuctl"
+Puppet::Type.type(:sensu_oidc_auth).provide(:sensu_api, :parent => Puppet::Provider::SensuAPI) do
+  desc "Provider sensu_oidc_auth using sensu API"
 
   mk_resource_methods
-
-  defaultfor :kernel => ['Linux','windows']
 
   def self.instances
     auths = []
 
-    data = sensuctl_list('auth', false)
-
-    auth_types = sensuctl_auth_types()
+    data = api_request('authproviders', nil, {:api_group => 'enterprise/authentication'})
     data.each do |d|
+      next unless d['type'] == 'oidc'
       auth = {}
       auth[:ensure] = :present
       auth[:name] = d['metadata']['name']
-      if auth_types[auth[:name]] != 'OIDC'
-        next
-      end
-      d.each_pair do |key,value|
+      d['spec'].each_pair do |key,value|
         next if key == 'metadata'
         if !!value == value
           value = value.to_s.to_sym
@@ -73,11 +67,16 @@ Puppet::Type.type(:sensu_oidc_auth).provide(:sensuctl, :parent => Puppet::Provid
       end
       spec[property] = value
     end
-    begin
-      sensuctl_create('oidc', metadata, spec, 'authentication/v2')
-    rescue Exception => e
-      raise Puppet::Error, "sensuctl create #{resource[:name]} failed\nError message: #{e.message}"
-    end
+    data = {}
+    data[:spec] = spec
+    data[:metadata] = metadata
+    data[:type] = 'oidc'
+    data[:api_version] = 'authentication/v2'
+    opts = {
+      :api_group => 'enterprise/authentication',
+      :method    => 'put',
+    }
+    api_request("authproviders/#{resource[:name]}", data, opts)
     @property_hash[:ensure] = :present
   end
 
@@ -100,21 +99,26 @@ Puppet::Type.type(:sensu_oidc_auth).provide(:sensuctl, :parent => Puppet::Provid
         end
         spec[property] = value
       end
-      begin
-        sensuctl_create('oidc', metadata, spec, 'authentication/v2')
-      rescue Exception => e
-        raise Puppet::Error, "sensuctl create #{resource[:name]} failed\nError message: #{e.message}"
-      end
+      data = {}
+      data[:spec] = spec
+      data[:metadata] = metadata
+      data[:type] = 'oidc'
+      data[:api_version] = 'authentication/v2'
+      opts = {
+        :api_group => 'enterprise/authentication',
+        :method    => 'put',
+      }
+      api_request("authproviders/#{resource[:name]}", data, opts)
     end
     @property_hash = resource.to_hash
   end
 
   def destroy
-    begin
-      sensuctl_delete('auth', resource[:name])
-    rescue Exception => e
-      raise Puppet::Error, "sensuctl delete auth #{resource[:name]} failed\nError message: #{e.message}"
-    end
+    opts = {
+      :api_group => 'enterprise/authentication',
+      :method    => 'delete',
+    }
+    api_request("authproviders/#{resource[:name]}", nil, opts)
     @property_hash.clear
   end
 end
---
+++ spec/unit/provider/sensu_oidc_auth/sensu_api_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:sensu_oidc_auth).provider(:sensu_api) do
+  let(:provider) { described_class }
+  let(:type) { Puppet::Type.type(:sensu_oidc_auth) }
+  let(:resource) do
+    type.new({
+      :name => 'oidc',
+      :client_id => 'id',
+      :client_secret => 'secret',
+      :server => 'https://idp.example.com',
+      :provider => 'sensu_api',
+    })
+  end
+
+  describe 'self.instances' do
+    it 'should create instances' do
+      allow(provider).to receive(:api_request).with('authproviders', nil, {:api_group => 'enterprise/authentication'}).and_return(JSON.parse(my_fixture_read('list.json')))
+      expect(provider.instances.length).to eq(1)
+    end
+
+    it 'should return the resource for a auth' do
+      allow(provider).to receive(:api_request).with('authproviders', nil, {:api_group => 'enterprise/authentication'}).and_return(JSON.parse(my_fixture_read('list.json')))
+      property_hash = provider.instances[0].instance_variable_get("@property_hash")
+      expect(property_hash[:name]).to eq('oidc')
+    end
+  end
+
+  describe 'create' do
+    it 'should create an auth' do
+      expected_spec = {
+        :spec => {
+          :client_id => 'id',
+          :client_secret => 'secret',
+          :server => 'https://idp.example.com',
+        },
+        :metadata => {
+          :name => 'oidc',
+        },
+        :api_version => 'authentication/v2',
+        :type => 'oidc',
+      }
+      expect(resource.provider).to receive(:api_request).with('authproviders/oidc', expected_spec, {:api_group => 'enterprise/authentication', :method => 'put'})
+      resource.provider.create
+      property_hash = resource.provider.instance_variable_get("@property_hash")
+      expect(property_hash[:ensure]).to eq(:present)
+    end
+  end
+
+  describe 'flush' do
+    it 'should update an auth config' do
+      expected_spec = {
+        :spec => {
+          :client_id => 'id',
+          :client_secret => 'secret',
+          :server => 'https://idp.example.com',
+          :username_claim => 'email',
+        },
+        :metadata => {
+          :name => 'oidc',
+        },
+        :api_version => 'authentication/v2',
+        :type => 'oidc',
+      }
+      expect(resource.provider).to receive(:api_request).with('authproviders/oidc', expected_spec, {:api_group => 'enterprise/authentication', :method => 'put'})
+      resource.provider.username_claim = 'email'
+      resource.provider.flush
+    end
+  end
+
+  describe 'destroy' do
+    it 'should delete an auth' do
+      expect(resource.provider).to receive(:api_request).with('authproviders/oidc', nil, {:api_group => 'enterprise/authentication', :method => 'delete'})
+      resource.provider.destroy
+      property_hash = resource.provider.instance_variable_get("@property_hash")
+      expect(property_hash).to eq({})
+    end
+  end
+end
+
