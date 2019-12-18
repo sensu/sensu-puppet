b292552d8a3e11c0892a664c13956d0ca4d19123
Trey Dockendorf <treydock@gmail.com>
2019-12-18 16:43:15 -0500

    Lint fixes

--- manifests/api.pp
+++ manifests/api.pp
@@ -3,21 +3,21 @@
 # Class to manage the Sensu API.
 #
 # @example
-#   include ::sensu::api
+#   include sensu::api
 #
 class sensu::api {
   include sensu
 
   sensu_api_config { 'sensu':
-    url          => $::sensu::api_url,
+    url          => $sensu::api_url,
     username     => 'admin',
-    password     => $::sensu::password,
-    old_password => $::sensu::old_password,
+    password     => $sensu::password,
+    old_password => $sensu::old_password,
   }
 
   sensu_api_validator { 'sensu':
-    sensu_api_server => $::sensu::api_host,
-    sensu_api_port   => $::sensu::api_port,
-    use_ssl          => $::sensu::use_ssl,
+    sensu_api_server => $sensu::api_host,
+    sensu_api_port   => $sensu::api_port,
+    use_ssl          => $sensu::use_ssl,
   }
 }
--- manifests/common.pp
+++ manifests/common.pp
@@ -4,55 +4,55 @@
 class sensu::common {
   include sensu
 
-  if $::sensu::etc_parent_dir {
+  if $sensu::etc_parent_dir {
     file { 'sensu_dir':
       ensure => 'directory',
-      path   => $::sensu::etc_parent_dir,
-      owner  => $::sensu::sensu_user,
-      group  => $::sensu::sensu_group,
-      mode   => $::sensu::directory_mode,
+      path   => $sensu::etc_parent_dir,
+      owner  => $sensu::sensu_user,
+      group  => $sensu::sensu_group,
+      mode   => $sensu::directory_mode,
     }
   }
 
   file { 'sensu_etc_dir':
     ensure  => 'directory',
-    path    => $::sensu::etc_dir,
-    owner   => $::sensu::sensu_user,
-    group   => $::sensu::sensu_group,
-    mode    => $::sensu::directory_mode,
-    purge   => $::sensu::etc_dir_purge,
-    recurse => $::sensu::etc_dir_purge,
-    force   => $::sensu::etc_dir_purge,
+    path    => $sensu::etc_dir,
+    owner   => $sensu::sensu_user,
+    group   => $sensu::sensu_group,
+    mode    => $sensu::directory_mode,
+    purge   => $sensu::etc_dir_purge,
+    recurse => $sensu::etc_dir_purge,
+    force   => $sensu::etc_dir_purge,
   }
 
-  if $::sensu::use_ssl {
+  if $sensu::use_ssl {
     contain sensu::ssl
   }
 
-  if $::sensu::manage_user and $::sensu::sensu_user {
+  if $sensu::manage_user and $sensu::sensu_user {
     user { 'sensu':
       ensure     => 'present',
-      name       => $::sensu::sensu_user,
+      name       => $sensu::sensu_user,
       forcelocal => true,
       shell      => '/bin/false',
-      gid        => $::sensu::sensu_group,
+      gid        => $sensu::sensu_group,
       uid        => undef,
       home       => '/var/lib/sensu',
       managehome => false,
       system     => true,
     }
   }
-  if $::sensu::manage_group and $::sensu::sensu_group {
+  if $sensu::manage_group and $sensu::sensu_group {
     group { 'sensu':
       ensure     => 'present',
-      name       => $::sensu::sensu_group,
+      name       => $sensu::sensu_group,
       forcelocal => true,
       gid        => undef,
       system     => true,
     }
   }
 
-  if $::sensu::manage_repo {
+  if $sensu::manage_repo {
     include sensu::repo
   }
 }
