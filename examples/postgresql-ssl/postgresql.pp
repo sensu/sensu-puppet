ec7e1c41ad83af8043f78bc048cdb51fc0428b50
Trey Dockendorf <treydock@gmail.com>
2020-03-27 16:35:01 -0400

    Add remote postgresql example using SSL

---
+++ examples/postgresql-ssl/postgresql.pp
@@ -0,0 +1,58 @@
+$password = 'sensu'
+
+class { 'sensu::agent':
+  backends => ['sensu-backend:8081'],
+}
+
+class { 'postgresql::globals':
+  manage_package_repo => true,
+  version             => '9.6',
+}
+
+class { 'postgresql::server':
+  listen_addresses => '*',
+}
+
+file { 'postgresql_ssl_key_file':
+  ensure => 'file',
+  path   => "${postgresql::server::datadir}/${trusted['certname']}.pem",
+  source => "/etc/puppetlabs/puppet/ssl/private_keys/${trusted['certname']}.pem",
+  owner  => 'postgres',
+  group  => 'postgres',
+  mode   => '0600',
+}
+
+postgresql::server::db { 'sensu':
+  user     => 'sensu',
+  password => postgresql_password('sensu', $password),
+}
+
+postgresql::server::pg_hba_rule { 'allow access to sensu database':
+  description => 'Open up postgresql for access to sensu from 0.0.0.0/0',
+  type        => 'host',
+  database    => 'sensu',
+  user        => 'sensu',
+  address     => '0.0.0.0/0',
+  auth_method => 'password',
+}
+
+postgresql::server::config_entry { 'ssl':
+  value => 'on',
+}
+
+postgresql::server::config_entry { 'ssl_cert_file':
+  value => "/etc/puppetlabs/puppet/ssl/certs/${trusted['certname']}.pem",
+}
+
+postgresql::server::config_entry { 'ssl_key_file':
+  value   => "${trusted['certname']}.pem",
+  require => File['postgresql_ssl_key_file'],
+}
+
+postgresql::server::config_entry { 'ssl_ca_file':
+  value => '/etc/puppetlabs/puppet/ssl/certs/ca.pem',
+}
+
+postgresql::server::config_entry { 'ssl_crl_file':
+  value => '/etc/puppetlabs/puppet/ssl/crl.pem',
+}
--- spec/acceptance/examples_spec.rb
+++ spec/acceptance/examples_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper_acceptance'
 
 describe 'examples', if: RSpec.configuration.sensu_full do
-  node = hosts_as('sensu-backend')[0]
+  agent = hosts_as('sensu-agent')[0]
+  backend = hosts_as('sensu-backend')[0]
 
   RSpec.configuration.sensu_examples.each do |example|
     it "should apply #{example} without errors" do
@@ -10,13 +11,39 @@ describe 'examples', if: RSpec.configuration.sensu_full do
         site_pp = "node 'sensu-backend' { #{pp} }"
         puppetserver = hosts_as('puppetserver')[0]
         create_remote_file(puppetserver, "/etc/puppetlabs/code/environments/production/manifests/site.pp", site_pp)
-        on node, puppet("agent -t --detailed-exitcodes"), acceptable_exit_codes: [0,2]
-        on node, puppet("agent -t --detailed-exitcodes"), acceptable_exit_codes: [0]
+        on backend, puppet("agent -t --detailed-exitcodes"), acceptable_exit_codes: [0,2]
+        on backend, puppet("agent -t --detailed-exitcodes"), acceptable_exit_codes: [0]
       else
         # Run it twice and test for idempotency
-        apply_manifest_on(node, pp, :catch_failures => true)
-        apply_manifest_on(node, pp, :catch_changes  => true)
+        apply_manifest_on(backend, pp, :catch_failures => true)
+        apply_manifest_on(backend, pp, :catch_changes  => true)
       end
+    end
+  end
+
+  context "PostgreSQL SSL examples" do
+    it "should apply without errors" do
+      agent_pp = File.read(File.join(RSpec.configuration.examples_dir, 'postgresql-ssl', 'postgresql.pp'))
+      backend_pp = File.read(File.join(RSpec.configuration.examples_dir, 'postgresql-ssl', 'sensu-backend.pp'))
+      if RSpec.configuration.sensu_use_agent
+        site_pp = "node 'sensu-backend' { #{backend_pp} }\nnode 'sensu-agent' { #{agent_pp} }"
+        puppetserver = hosts_as('puppetserver')[0]
+        create_remote_file(puppetserver, "/etc/puppetlabs/code/environments/production/manifests/site.pp", site_pp)
+        on agent, puppet("agent -t --detailed-exitcodes"), acceptable_exit_codes: [0,2]
+        on agent, puppet("agent -t --detailed-exitcodes"), acceptable_exit_codes: [0]
+        on backend, puppet("agent -t --detailed-exitcodes"), acceptable_exit_codes: [0,2]
+        on backend, puppet("agent -t --detailed-exitcodes"), acceptable_exit_codes: [0]
+      else
+        apply_manifest_on(agent, agent_pp, :catch_failures => true)
+        apply_manifest_on(agent, agent_pp, :catch_changes  => true)
+        apply_manifest_on(backend, backend_pp, :catch_failures => true)
+        apply_manifest_on(backend, backend_pp, :catch_changes  => true)
+      end
+      sleep 5
+    end
+
+    describe command("PGPASSWORD='sensu' psql -U sensu -h sensu-agent -c \"select * from events WHERE sensu_check = 'keepalive' LIMIT 1;\""), :node => backend do
+      its(:stdout) { should contain('keepalive') }
     end
   end
 end
