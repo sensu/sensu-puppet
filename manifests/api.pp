a08a7f2b4b044c89f7260c2f5b6a29b04917a4b0
Geir <geir.tufte@kantega.no>
2022-11-07 13:50:24 +0100

    Adding the option to opt out on entity validation

--- manifests/api.pp
+++ manifests/api.pp
@@ -15,10 +15,12 @@ class sensu::api {
     validate_namespaces => $sensu::validate_namespaces,
   }
 
-  sensu_api_validator { 'sensu':
-    ensure           => 'present',
-    sensu_api_server => $sensu::api_host,
-    sensu_api_port   => $sensu::api_port,
-    use_ssl          => $sensu::use_ssl,
+  if ! $sensu::agent::agent_managed_entity {
+    sensu_api_validator { 'sensu':
+      ensure           => 'present',
+      sensu_api_server => $sensu::api_host,
+      sensu_api_port   => $sensu::api_port,
+      use_ssl          => $sensu::use_ssl,
+    }
   }
 }
