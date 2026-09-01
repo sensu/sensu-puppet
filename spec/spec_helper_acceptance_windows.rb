63466af9e5c4e8a901c85b640514eedc6c156113
Sourabh Patel <sourabh.patel.ctr@sumologic.com>
2026-09-02 01:47:39 +0530

    🔧chore(windows): remove manual puppet agent installation and path hacking
    Signed-off-by: Sourabh Patel <sourabh.patel.ctr@sumologic.com>

--- spec/spec_helper_acceptance_windows.rb
+++ spec/spec_helper_acceptance_windows.rb
@@ -2,16 +2,6 @@ require 'serverspec'
 
 set :backend, :cmd
 
-if Gem.win_platform?
-  # bundle exec prepends vendor/bundle/ruby/3.4.0/bin to PATH, so the bundled
-  # puppet binstub wins over system puppet. Prepend the system Puppet bin dir
-  # so tests invoke the installed puppet-agent (with its own embedded Ruby and
-  # native ffi) rather than the gem from the test bundle.
-  # Also clear RUBYOPT so puppet's embedded Ruby doesn't inherit -r bundler/setup.
-  ENV['PATH'] = "C:\\Program Files\\Puppet Labs\\Puppet\\bin#{File::PATH_SEPARATOR}#{ENV['PATH']}"
-  ENV.delete('RUBYOPT')
-end
-
 RSpec.configure do |c|
   c.formatter = :documentation
 end
